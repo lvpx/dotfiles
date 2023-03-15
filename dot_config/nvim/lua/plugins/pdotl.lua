5f3424910ecd34df171293a23c4ed334598c821c
Lovepreet <lovepreetkaul23@gmail.com>
2023-03-15 23:59:11 +0530

    Make edits to Lazyvim

---
+++ dot_config/nvim/lua/plugins/pdotl.lua
@@ -0,0 +1,46 @@
+return {
+  -- add gruvbox
+  { "ellisonleao/gruvbox.nvim" },
+
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "gruvbox",
+    },
+  },
+
+  -- change trouble config
+  {
+    "folke/trouble.nvim",
+    -- opts will be merged with the parent spec
+    opts = { use_diagnostic_signs = true },
+  },
+
+  -- disable trouble
+  -- { "folke/trouble.nvim", enabled = false },
+  { import = "lazyvim.plugins.extras.lang.typescript" },
+
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "bash",
+        "help",
+        "html",
+        "javascript",
+        "json",
+        "lua",
+        "markdown",
+        "markdown_inline",
+        "python",
+        "query",
+        "regex",
+        "tsx",
+        "typescript",
+        "vim",
+        "yaml",
+      },
+    },
+  },
+}

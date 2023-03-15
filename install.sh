8e8934cdbad31e6b772f9e16a8d539de21a71944
Lovepreet <lovepreet@192.168.1.6>
2023-03-15 23:23:01 +0530

    Update install.sh with TODO comments

--- install.sh
+++ install.sh
@@ -4,5 +4,14 @@ set -e # -e: exit on error
 
 GITHUB_USERNAME="pdotl"
 
-# Install and apply chezmoi
+# Check which OS
+
+# Install Homebrew if mac
+
+# Install Neovim based on OS
+
+# Install and apply chezmoi regardless
 sh -c "$(curl -fsLS get.chezmoi.io)" -- init --apply $GITHUB_USERNAME
+
+# Take action based on codespace or not
+# Install ripgrep, lazygit

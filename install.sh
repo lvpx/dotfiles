0d52f2046653d9fcb192577d760ac94de1a94b0a
Lovepreet Singh <pdotl@github.com>
2023-03-17 18:21:19 +0530

    Fix install.sh errors

--- install.sh
+++ install.sh
@@ -5,20 +5,18 @@ set -e # -e: exit on error
 GITHUB_USERNAME="pdotl"
 
 # Install Homebrew if mac
-if [[ "$OSTYPE" == "darwin"* ]]; then
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-fi]
+if [[ "$OSTYPE" = "darwin"* ]]; then
+	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
 
 # Ensure neovim is installed. brew works on both Codespaces and mac
-if ! command -v nvim &> /dev/null; then
-  brew install neovim
-fi
+[[ ! command -v nvim ]] && brew install neovim
 
 # Install and apply chezmoi regardless
 sh -c "$(curl -fsLS get.chezmoi.io)" -- init --apply $GITHUB_USERNAME
 
 # Install ripgrep, lazygit, shellcheck on codespaces
-[[ ! -z "$CODESPACES" ]] && apt install -y ripgrep lazygit shellcheck
+[[ ! -z "$CODESPACES" ]] && apt install -y ripgrep lazygit
 
 # Change shell to zsh
 if ! grep -q "root.*/bin/zsh" /etc/passwd; then

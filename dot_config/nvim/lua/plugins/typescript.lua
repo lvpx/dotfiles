f180d36ebd62281b25ea5471974d7b3c8998e5dd
Lovepreet Singh <pdotl@github.com>
2023-03-17 12:37:07 +0530

    Add separate config for typescript

--- dot_config/nvim/lua/plugins/typescript.lua
+++ dot_config/nvim/lua/plugins/typescript.lua
@@ -1,42 +1,48 @@
 return {
+  { "jose-elias-alvarez/typescript.nvim" },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      if type(opts.ensure_installed) == "table" then
+        vim.list_extend(opts.ensure_installed, { "typescript", "tsx" })
+      end
+    end,
+  },
   {
     "neovim/nvim-lspconfig",
-    -- other settings removed for brevity
+    dependencies = { "jose-elias-alvarez/typescript.nvim" },
     opts = {
+      -- make sure mason installs the server
       servers = {
-        eslint = {
+        ---@type lspconfig.options.tsserver
+        tsserver = {
           settings = {
-            -- helps eslint find the eslintrc when it's placed in a subfolder instead of the cwd root
-            workingDirectory = { mode = "auto" },
+            completions = {
+              completeFunctionCalls = true,
+            },
           },
         },
       },
       setup = {
-        eslint = function()
-          vim.api.nvim_create_autocmd("BufWritePre", {
-            callback = function(event)
-              if require("lspconfig.util").get_active_client_by_name(event.buf, "eslint") then
-                vim.cmd("EslintFixAll")
-              end
-            end,
-          })
+        tsserver = function(_, opts)
+          require("lazyvim.util").on_attach(function(client, buffer)
+            if client.name == "tsserver" then
+            -- stylua: ignore
+            vim.keymap.set("n", "<leader>co", "<cmd>TypescriptOrganizeImports<CR>", { buffer = buffer, desc = "Organize Imports" })
+            -- stylua: ignore
+            vim.keymap.set("n", "<leader>cR", "<cmd>TypescriptRenameFile<CR>", { desc = "Rename File", buffer = buffer })
+            end
+          end)
+          require("typescript").setup({ server = opts })
+          return true
         end,
-      },
-    },
-  },
-  {
-    "williamboman/mason.nvim",
-    opts = {
-      ensure_installed = {
-        "prettierd",
       },
     },
   },
   {
     "jose-elias-alvarez/null-ls.nvim",
     opts = function(_, opts)
-      local nls = require("null-ls")
-      table.insert(opts.sources, nls.builtins.formatting.prettierd)
+      table.insert(opts.sources, require("typescript.extensions.null-ls.code-actions"))
     end,
   },
 }

26be75f15b21b49d2b1ce9f4ebd3113e8ab765e9
Lovepreet Singh <pdotl@github.com>
2023-03-17 16:44:31 +0530

    Final touches

--- install.sh
+++ install.sh
@@ -4,18 +4,23 @@ set -e # -e: exit on error
 
 GITHUB_USERNAME="pdotl"
 
-# Check which OS
-
 # Install Homebrew if mac
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi]
 
-# Install Neovim based on OS
+# Ensure neovim is installed. brew works on both Codespaces and mac
+if ! command -v nvim &> /dev/null; then
+  brew install neovim
+fi
 
 # Install and apply chezmoi regardless
 sh -c "$(curl -fsLS get.chezmoi.io)" -- init --apply $GITHUB_USERNAME
 
-# Take action based on codespace or not
-# Install ripgrep, lazygit
+# Install ripgrep, lazygit, shellcheck on codespaces
+[[ ! -z "$CODESPACES" ]] && apt install -y ripgrep lazygit shellcheck
 
-# Ensure zsh is used
-chsh -s /bin/zsh
-source ~/.zshrc
+# Change shell to zsh
+if ! grep -q "root.*/bin/zsh" /etc/passwd; then
+	chsh -s /bin/zsh root
+fi

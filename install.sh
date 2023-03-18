bd4564c144546beb146be9f38c8e09cadad27090
Lovepreet Singh <pdotl@github.com>
2023-03-18 16:04:43 +0530

    Simplify zsh setup

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ if [ -z "$USER" ]; then
 	USER=$(id -un)
 fi
 cd "$HOME"
+
 # Make passwordless sudo work
 export SUDO_ASKPASS=/bin/true
 
@@ -21,12 +22,13 @@ fi
 
 # Install ripgrep, lazygit, shellcheck on codespaces
 if [[ -n "$CODESPACES" ]]; then
-	sudo apt update 2>&1 1>/dev/null && sudo apt-get install --no-install-recommends -y ripgrep >&1 1>/dev/null
+	sudo apt update 1>/dev/null &
+	sudo apt-get install --no-install-recommends -y ripgrep >&1 1>/dev/null
 fi
 
 # Change shell to zsh
 if [[ "$SHELL" != "/usr/bin/zsh" ]]; then
-	sudo chsh -s "/bin/zsh" "$USER"
+	sudo chsh -s "/usr/bin/zsh" "$USER"
 fi
 
 # Install and apply chezmoi regardless
